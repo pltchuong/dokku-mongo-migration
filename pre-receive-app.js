51a6582b8cd87bb55d8ad6a50136e250636f7670
Phan Chuong <pltchuong@gmail.com>
2016-05-09 21:02:13 -0700

    update build data

--- pre-receive-app.js
+++ pre-receive-app.js
@@ -1,3 +1,24 @@
 (function() {
   printjson('pre-receive-app ' + parameters);
+
+  var url = 'apps.solutionsresource.com';
+  var now = new Date(),
+      parsed = parameters.match(/([^\s]*)\s([^\s]*)\s([^\s]*)\s(.*)/),
+      name = parsed[1],
+      app = db.apps.findOne({name: name}),
+      type = parsed[2],
+      tempdir = parsed[3],
+      commit = parsed[4],
+      build = {
+        app: app._id,
+        type: type,
+        commit: commit,
+        status: 'running'
+      };
+
+  build.created_at = now;
+  build.updated_at = now;
+
+  app.builds.save(build);
+
 })();
--- receive-app.js
+++ receive-app.js
@@ -1,3 +1,19 @@
 (function() {
   printjson('receive-app ' + parameters);
+
+  var url = 'apps.solutionsresource.com';
+  var now = new Date(),
+      parsed = parameters.match(/(.*)/),
+      name = parsed[1],
+      app = db.apps.findOne({name: name}),
+      build = db.apps.findOne({
+        app: app._id,
+        status: 'running'
+      });
+
+  build.status = 'succeeded';
+  build.updated_at = now;
+
+  app.builds.save(build);
+
 })();
