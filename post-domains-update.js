6fe7bef4e0b539f277cfceaa13251e65d3b294a6
Phan Chuong <pltchuong@gmail.com>
2016-05-09 20:22:13 -0700

    fix clear domains

--- post-domains-update.js
+++ post-domains-update.js
@@ -3,7 +3,7 @@
 
   var url = 'apps.solutionsresource.com';
   var now = new Date(),
-      parsed = parameters.match(/([^\s]*)\s([^\s]*)\s(.*)/),
+      parsed = parameters.match(/([^\s]*)\s?([^\s]*)?\s?(.*)?/),
       name = parsed[1],
       app = db.apps.findOne({name: name}),
       action = parsed[2],
@@ -42,14 +42,17 @@
     case 'clear':
       db.domains.remove({_id: {$in: app.domains}, cname: {$ne: null}});
 
+      domain = db.domains.findOne({hostname: cname});
       app.domains = app.domains.filter(function(tmp) {
-        return tmp.valueOf() != cname;
+        return tmp.valueOf() == domain._id.valueOf();
       });
       app.updated_at = now;
       db.apps.save(app);
       break;
 
     default:
+      // plugins/certs/subcommands/remove
+      // plugn trigger post-domains-update "$APP"
       break;
   }
   
