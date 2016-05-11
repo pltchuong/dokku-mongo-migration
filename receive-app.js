54db6b59468fdc4d2648914392bcb48e56b65e83
Phan Chuong <pltchuong@gmail.com>
2016-05-11 01:18:08 -0700

    redundant hook since we have build logger

--- receive-app.js
+++ receive-app.js
@@ -1,20 +1,3 @@
 (function() {
   printjson('receive-app ' + parameters);
-
-  var url = 'apps.solutionsresource.com';
-  var now = new Date(),
-      parsed = parameters.match(/([^\s]*)\s?(.*)?/),
-      name = parsed[1],
-      app = db.apps.findOne({name: name}),
-      commit = parsed[2],
-      build = db.builds.findOne({
-        app: app._id,
-        status: 'running'
-      });
-
-  build.status = 'succeeded';
-  build.updated_at = now;
-
-  db.builds.save(build);
-
 })();
