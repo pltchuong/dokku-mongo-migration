51c820541aa6890d10dba6a36b640d0ece7fd41c
Phan Chuong <pltchuong@gmail.com>
2016-05-11 21:44:45 -0700

    finish handling stderr and stdout

--- build-logger.js
+++ build-logger.js
@@ -3,21 +3,15 @@
 
   var url = 'apps.solutionsresource.com';
   var now = new Date(),
-      parsed = parameters.match(/([^\s]*)\s(.*)/),
+      parsed = parameters.match(/([^\s]*)\s([^\s]*)\s([^\s]*)\s(.*)/),
       name = parsed[1],
       app = db.apps.findOne({name: name}),
-      log = parsed[2],
+      uuid = parsed[2],
+      type = parsed[3],
+      log = parsed[4],
       build = {
         _id: uuid,
         app: app._id
-      },
-      trimBuffer = function(buffer) {
-        for (var i = 0; i < buffer.length; i++) {
-          if (buffer[i] === 0x1B) {
-            return trimBuffer(buffer.slice(i + 4));
-          }
-        }
-        return buffer;
       };
 
   if(log === '*****start*****') {
@@ -25,18 +19,23 @@
     build.updated_date = now;
     build.status = 'started';
     build.output = '';
+    build.error = '';
     db.builds.save(build);
   } else if(log === '*****end*****') {
     build = db.builds.findOne(build);
     build.updated_date = now;
-    build.status = 'finished';
-    build.output = 
+    build.status = build.error ? 'failed' : 'finished';
     db.builds.save(build);
   } else {
     build = db.builds.findOne(build);
-    build.updated_date = now;
     build.status = 'running';
-    build.output += trimBuffer(log) + "\n";
+    if(type === 'out') {
+      build.output += log + "\n";
+    }
+    if(type === 'err') {
+      build.error += log + "\n";
+    }
+    build.updated_date = now;
     db.builds.save(build);
   }
 })();
