074324e34d56e16bd17223ef747c9293517a01d8
Phan Chuong <pltchuong@gmail.com>
2016-06-10 13:33:15 -0700

    remove unnecessary uuid as parameter

--- logger.js
+++ logger.js
@@ -5,22 +5,16 @@
 
   var url = 'apps.solutionsresource.com';
   var now = new Date(),
-      parsed = parameters.match(/([^\s]*)\s?(.*)?/),
-      uuid = parsed[1],
-      log = parsed[2] || '',
+      log = parameters,
       activity = db.activities.findOne({_id: uuid});
-
-  activity.status = 'started';
-  activity.updated_at = now;
 
   log = log.replace(/[ ]*\^\[\[1G/g, '');
   log = log.replace(/00[0-9a-z]{2,}\^[A-Z]/g, '');
   activity.output += log + '\n';
+  activity.updated_at = now;
 
-  if(activity.output.indexOf('pre-receive hook declined') >= 0) {
-    activity.status = 'failed';
-  } else if(activity.output.indexOf('Application deployed') >= 0) {
-    activity.status = 'finished';
+  if(activity.output.includes('*****end*****')) {
+    activity.status = 'ended';
   } else {
     activity.status = 'running';
   }
