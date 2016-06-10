44c2de5eec7104bc46d1497fc4b420bf862afe6f
Phan Chuong <pltchuong@gmail.com>
2016-06-10 10:05:39 -0700

    re #3

--- logger.js
+++ logger.js
@@ -10,16 +10,13 @@
       log = parsed[2] || '',
       activity = db.activities.findOne({_id: uuid});
 
-  log = log.replace(/[ ]*\^\[\[1G/g, '');
-  log = log.replace(/00[0-9a-z]{2,}\^[A-Z]/g, '');
-
   activity.status = 'started';
   activity.updated_at = now;
-  if(activity.output.indexOf(log.trim()) < 0) {
-    activity.output += log + '\n';
-  } else {
-    activity.output.replace(log.trim(), log);
-  }
+
+  log = log.replace(/[ ]*\^\[\[1G/g, '');
+  log = log.replace(/00[0-9a-z]{2,}\^[A-Z]/g, '');
+  activity.output += log + '\n';
+
   if(activity.output.indexOf('pre-receive hook declined') >= 0) {
     activity.status = 'failed';
   } else if(activity.output.indexOf('Application deployed') >= 0) {
